e670a927cc186fb9d3006a19442aca9f183c2d8e
Dumitru Gorgan <dgorgan@adobe.com>
2025-04-23 09:47:20 -0700

    initial commit: setup project and README

---
+++ README.md
@@ -0,0 +1,35 @@
+# Couchsurfing Local
+
+A simple web app to browse a feed of posts, and view the individual posts and profiles of users who made the posts.
+
+## Features
+
+- Browse the user feed and view individual posts
+- Explore user profiles and their details
+- Navigate from posts and profiles back to feed
+
+## Setup
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/dgorgan/couchsurfing-social-demo.git
+   cd couchsurfing-local-demo
+   ```
+
+2. Install the dependencies:
+
+   ```bash
+   npm install
+   ```
+
+3. Run dev server
+   ```bash
+   npm run dev
+   ```
+
+## Technologies
+
+- **Next.js**:
+- **Tailwind CSS**:
+- **TypeScript**:
---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div
+      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          src="https://www.couchsurfing.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo-full.b9a1e2af.png&w=256&q=75"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+       Hello World
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        2025
+      </footer>
+    </div>
+  );
+}
